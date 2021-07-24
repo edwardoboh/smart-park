c9b7c60c0eb915fe5d514277f8fe5423545269d0
edwardoboh <obohedward@gmail.com>
2021-07-23 23:37:42 -0700

    refactor server url

--- client/src/data/actions.js
+++ client/src/data/actions.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
+import serverUrl from '../url'
 
 const config = {
     "Content-Type": "application/json"
 }
 
 export const getParks = ({setPark}) => {
-    // axios.get("http://localhost:5000").then(resp => {
-    axios.get("http://localhost:5000").then(resp => {
+    axios.get(serverUrl).then(resp => {
         console.log("Axios Response: ", resp.data)
         setPark(resp.data)
     }).catch(err => {
